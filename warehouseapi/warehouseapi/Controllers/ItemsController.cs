74ea54bcd3d897664265318e775024eb79188f1d
bakpawel <pawel.bak@ttpsc.pl>
2022-06-28 18:52:20 +0200

    Initializing project

---
+++ warehouseapi/warehouseapi/Controllers/ItemsController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using System.Xml.Linq;
+using warehouseapi.Databases;
+using warehouseapi.Models;
+using static warehouseapi.Models.Item;
+
+namespace warehouseapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        public const string ItemsDbPath = "./Databases/items.xml";
+
+        // GET: api/<ItemsController>
+        [HttpGet]
+        public IEnumerable<Item> Get()
+        {
+            return ItemsDatabaseController.LoadItemsDatabase(ItemsDbPath);
+        }
+
+        // GET api/<ItemsController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Item> Get(Guid id)
+        {
+            List<Item>? items = ItemsDatabaseController.LoadItemsDatabase(ItemsDbPath);
+            if(items == null)
+            {
+                return NotFound();
+            }
+
+            Item? item = items.FirstOrDefault(item => item.Id == id.ToString());
+
+            return item != null ? item : NotFound();
+        }
+
+        // POST api/<ItemsController>
+        [HttpPost]
+        public List<Item> Post([FromBody] ItemBody itemBody)
+        {
+            List<Item> items = ItemsDatabaseController.LoadItemsDatabase(ItemsDbPath);
+            items.Add(new Item(itemBody));
+
+            XDocument root = new XDocument(
+                new XElement("items",
+                    items.Select(item =>
+                        new XElement("item",
+                            new XAttribute("id", item.Id),
+                            new XAttribute("name", item.Name ?? ""),
+                            new XAttribute("createDate", item.CreateDate.ToString() ?? ""),
+                            new XAttribute("description", item.Description ?? ""),
+                            new XAttribute("quantity", item.Quantity),
+                            new XAttribute("price", item.Price)
+                        )
+                    )
+                )
+            );
+
+            using (StreamWriter writer = new StreamWriter(ItemsDbPath))
+            {
+                writer.Write(root.ToString());
+            }
+
+            return items;
+        }
+
+        // PUT api/<ItemsController>/5
+        [HttpPut("{id}")]
+        public ActionResult<List<Item>> Put(Guid id, [FromBody] ItemBody item)
+        {
+            List<Item>? items = ItemsDatabaseController.LoadItemsDatabase(ItemsDbPath);
+            if (items == null)
+            {
+                return NotFound();
+            }
+
+            Item foundItem = items.FirstOrDefault(item => item.Id == id.ToString());
+            if(foundItem == null)
+            {
+                return NotFound();
+            }
+
+            XDocument root = new XDocument(
+                new XElement("items",
+                    items.Select(item =>
+                        new XElement("item",
+                            new XAttribute("id", item.Id),
+                            new XAttribute("name", item.Name ?? ""),
+                            new XAttribute("createDate", item.CreateDate.ToString() ?? ""),
+                            new XAttribute("description", item.Description ?? ""),
+                            new XAttribute("quantity", item.Quantity),
+                            new XAttribute("price", item.Price)
+                        )
+                    )
+                )
+            );
+
+            using (StreamWriter writer = new StreamWriter(ItemsDbPath))
+            {
+                writer.Write(root.ToString());
+            }
+
+            return items;
+        }
+
+        // DELETE api/<ItemsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(Guid id)
+        {
+
+        }
+    }
+}
---
+++ warehouseapi/warehouseapi/Models/Item.cs
@@ -0,0 +1,37 @@
+﻿namespace warehouseapi.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string Description { get; set; }
+        public float Quantity { get; set; }
+        public float Price { get; set; }
+
+        public Item()
+        {
+            Id = Guid.NewGuid().ToString();
+            Name = String.Empty;
+            Description = String.Empty;
+        }
+
+        public Item(ItemBody itemBody)
+        {
+            Id = Guid.NewGuid().ToString();
+            Name = itemBody.Name;
+            CreateDate = DateTime.Now;
+            Description = itemBody.Description;
+            Quantity = itemBody.Quantity;
+            Price = itemBody.Price;
+        }
+
+        public class ItemBody
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public float Quantity { get; set; }
+            public float Price { get; set; }
+        }
+    }
+}
